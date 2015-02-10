6f5850771f7fdfc190f14a36f31ff2b20b9edd95
Anton <anthonyla2@gmail.com>
2015-02-11 00:46:22 +0200

    + unnecessary lines are cleaned

--- internetShop/database/databaseCreationScript.sql
+++ internetShop/database/databaseCreationScript.sql
@@ -5,7 +5,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE SCHEMA IF NOT EXISTS `Java2_test` DEFAULT CHARACTER SET utf8 ;
 USE `Java2_test` ;
 
---- Removing table if it already exists
+-- Removing table if it already exists
 DROP TABLE IF EXISTS `java2_test`.`users` ;
 DROP TABLE IF EXISTS `java2_test`.`addresses` ;
 DROP TABLE IF EXISTS `java2_test`.`clients` ;
@@ -13,13 +13,14 @@ DROP TABLE IF EXISTS `java2_test`.`emails` ;
 DROP TABLE IF EXISTS `java2_test`.`phones` ;
 DROP TABLE IF EXISTS `java2_test`.`products` ;
 
---- Create all tables again
+-- Create all tables again 
 CREATE TABLE IF NOT EXISTS `java2_test`.`users` (
   `UserID` INT(11) NOT NULL AUTO_INCREMENT,
   `FirstName` CHAR(32) NOT NULL,
   `LastName` CHAR(32) NOT NULL,
   PRIMARY KEY (`UserID`)
 )
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `java2_test`.`addresses` (
   `AddressID` INT(11) NOT NULL AUTO_INCREMENT,
@@ -32,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `java2_test`.`addresses` (
   `Postcode` CHAR(7) NOT NULL,
   PRIMARY KEY (`AddressID`)
 )
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `java2_test`.`clients` (
   `ClientID` INT(11) NOT NULL AUTO_INCREMENT,
@@ -41,6 +43,7 @@ CREATE TABLE IF NOT EXISTS `java2_test`.`clients` (
   `Gender` CHAR(6) NOT NULL,
   PRIMARY KEY (`ClientID`)
 )
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `java2_test`.`emails` (
   `EmailID` INT(11) NOT NULL AUTO_INCREMENT,
@@ -48,6 +51,7 @@ CREATE TABLE IF NOT EXISTS `java2_test`.`emails` (
   `Email_Address` CHAR(32) NOT NULL,
   PRIMARY KEY (`EmailID`)
 )
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `java2_test`.`phones` (
   `PhoneID` INT(11) NOT NULL AUTO_INCREMENT,
@@ -55,6 +59,7 @@ CREATE TABLE IF NOT EXISTS `java2_test`.`phones` (
   `PhoneNumber` CHAR(32) NOT NULL,
   PRIMARY KEY (`PhoneID`)
 )
+ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `java2_test`.`products` (
   `ProductID` INT(11) NOT NULL AUTO_INCREMENT,
@@ -63,10 +68,7 @@ CREATE TABLE IF NOT EXISTS `java2_test`.`products` (
   `Price` decimal(20,6) unsigned NOT NULL,
   PRIMARY KEY (`ProductID`)
 )
-
---- Start ID = 0
-ENGINE = InnoDB
-AUTO_INCREMENT = 0;
+ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
