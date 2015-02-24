6fa65c8f2682438f25b529e9a1cca62ca9fb4f69
Aleksandrs Zaharacenko <toweb7@gmail.com>
2015-02-24 19:20:10 +0200

    Implemented spring for MVCFIlter

--- internetShop/src/main/java/lv/javaguru/java2/servlet/mvc/MVCFilter.java
+++ internetShop/src/main/java/lv/javaguru/java2/servlet/mvc/MVCFilter.java
@@ -2,6 +2,10 @@ package lv.javaguru.java2.servlet.mvc;
 
 import lv.javaguru.java2.AccessLevel;
 import lv.javaguru.java2.servlet.mvc.models.MVCModel;
+import lv.javaguru.java2.servlet.mvc.spring.SpringAppConfig;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,30 +14,48 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by alex on 17/02/15.
  */
 public class MVCFilter implements Filter {
 
+    private static Logger logger = Logger.getLogger(MVCFilter.class.getName());
+
+    private ApplicationContext springContext;
+
     private Map<String, MVCController> controllerMapping;
 
     @Override
     public void init (FilterConfig filterConfig) throws ServletException {
         controllerMapping = new HashMap<String, MVCController>();
-        controllerMapping.put("/index", new IndexController());
-        controllerMapping.put("/users", new UsersController());
-        controllerMapping.put("/register", new RegisterController());
-        controllerMapping.put("/login", new LoginController());
-        controllerMapping.put("/logout", new LoginController());
-        controllerMapping.put("/about", new AboutCompanyController());
-        controllerMapping.put("/cart", new CartController());
+        controllerMapping.put("/index", getBean(IndexController.class));
+        controllerMapping.put("/users", getBean(UsersController.class));
+        controllerMapping.put("/register", getBean(RegisterController.class));
+        controllerMapping.put("/login", getBean(LoginController.class));
+        controllerMapping.put("/logout", getBean(LoginController.class));
+        controllerMapping.put("/about", getBean(AboutCompanyController.class));
+        controllerMapping.put("/cart", getBean(CartController.class));
+    }
+
+    private MVCController getBean(Class clazz){
+        return (MVCController) springContext.getBean(clazz);
     }
 
     @Override
     public void doFilter(ServletRequest request,
                          ServletResponse response,
                          FilterChain filterChain) throws IOException, ServletException {
+
+        try {
+            springContext =
+                    new AnnotationConfigApplicationContext(SpringAppConfig.class);
+        } catch (BeansException e) {
+            logger.log(Level.INFO, "Spring context failed to start", e);
+        }
+
 
         HttpServletRequest req = (HttpServletRequest)request;
         HttpServletResponse resp = (HttpServletResponse)response;
---
+++ internetShop/src/main/java/lv/javaguru/java2/servlet/mvc/spring/SpringAppConfig.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java2.servlet.mvc.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"lv.javaguru.java2"})
+public class SpringAppConfig {
+}
