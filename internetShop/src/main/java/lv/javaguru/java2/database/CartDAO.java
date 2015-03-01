d86ab436dbb51dce2cf10bb91ae44ae134d60c60
Anton <anthonyla2@gmail.com>
2015-03-02 00:33:27 +0200

    + cart table added in DB

---
+++ internetShop/src/main/java/lv/javaguru/java2/database/CartDAO.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java2.database;
+
+/**
+ * Created by Anton on 2015.03.02..
+ */
+public interface CartDAO {
+    //void createElem(Long productID, Long userID, Integer count) throws DBException;
+    //HashMap<Product, Integer> getUserCart(Long userID) throws DBException;
+    //void updateElem(Long productID, Long userID, Integer toAdd) throws DBException;
+}
