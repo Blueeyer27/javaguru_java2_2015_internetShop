51f7b67222c63f2b99bfd1286c8bf597832614de
Anton <anthonyla2@gmail.com>
2015-03-03 20:23:42 +0200

    + Product domain Entity

--- internetShop/src/main/java/lv/javaguru/java2/domain/Product.java
+++ internetShop/src/main/java/lv/javaguru/java2/domain/Product.java
@@ -1,11 +1,37 @@
 package lv.javaguru.java2.domain;
 
-public class Product {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "products")
+public class Product {
+    /*
+    CREATE TABLE IF NOT EXISTS `java2_test`.`products` (
+      `ProductID` INT(11) NOT NULL AUTO_INCREMENT,
+      `Name` CHAR(32) NOT NULL,
+      `Description` TEXT NOT NULL,
+      `Price` DECIMAL(20,6) UNSIGNED NOT NULL,
+      `Picture` CHAR(255) NULL DEFAULT NULL,
+      PRIMARY KEY (`ProductID`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8;
+     */
+
+    @Column(name="ProductID",columnDefinition = "INT(11)")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected long productId;
+
+    @Column(name = "Name", columnDefinition = "CHAR(32)")
     protected String name;
+
+    @Column(name = "Description", columnDefinition = "TEXT")
     protected String description;
+
+    @Column(name = "Price", columnDefinition = "DECIMAL(20,6)")
     protected float price;
+
+    @Column(name = "Picture", columnDefinition = "CHAR(255)")
     protected String imageURL;
 
     public Product() { }
