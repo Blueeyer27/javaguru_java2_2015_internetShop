3fe4ee171349a3a836e39b7a7cbdcd42f0a355af
Anton <anthonyla2@gmail.com>
2015-03-23 03:00:42 +0200

    small MockFileItem fixes

--- internetShop/src/test/java/lv/javaguru/java2/mock/MockFileItem.java
+++ internetShop/src/test/java/lv/javaguru/java2/mock/MockFileItem.java
@@ -4,8 +4,6 @@ package lv.javaguru.java2.mock;
  * Created by Anton on 2015.03.23..
  */
 
-import org.apache.commons.fileupload.FileItem;
-
 import java.io.*;
 import java.nio.charset.Charset;
 
@@ -40,42 +38,52 @@ public class MockFileItem implements FileItem {
         this.content = content;
     }
 
+    @Override
     public void delete() {
 
     }
 
+    @Override
     public byte[] get() {
         return content;
     }
 
+    @Override
     public String getContentType() {
         return contentType;
     }
 
+    @Override
     public String getFieldName() {
         return fieldName;
     }
 
+    @Override
     public InputStream getInputStream() throws IOException {
         return new ByteArrayInputStream(content);
     }
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public OutputStream getOutputStream() throws IOException {
         return null;
     }
 
+    @Override
     public long getSize() {
         return content == null ? 0 : content.length;
     }
 
+    @Override
     public String getString() {
         return new String(content);
     }
 
+    @Override
     public String getString(String charsetName) throws UnsupportedEncodingException {
         try {
             return new String(content, Charset.forName(charsetName));
@@ -84,24 +92,28 @@ public class MockFileItem implements FileItem {
         }
     }
 
+    @Override
     public boolean isFormField() {
         return formField;
     }
 
+    @Override
     public boolean isInMemory() {
         return false;
     }
 
+    @Override
     public void setFieldName(String arg0) {
 
     }
 
+    @Override
     public void setFormField(boolean arg0) {
 
     }
 
-    public void write(File arg0)
-            throws Exception {
+    @Override
+    public void write(File arg0) throws Exception {
 
     }
 
